bc5755f5665cbb90951d8b81d88105d9b72fe43e
thierryraimond <thierry.raimond@wanadoo.fr>
2018-03-18 20:23:36 +0100

    update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
-var webpackConfig = require('./webpack.config.js');
+const webpackConfig = require('./webpack.config.js');
 
-module.exports = function(config) {
+module.exports = config => {
     config.set({
         basePath: '',
         frameworks: ['jasmine'],
